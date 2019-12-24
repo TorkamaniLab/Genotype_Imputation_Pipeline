c57d9511e4f06363607516ac58a85928e2567784
Shaun Chen <38847347+ShaunFChen@users.noreply.github.com>
2019-12-24 03:06:44 -0800

    update task scheduler
    bash fun

--- generate_commands_automatically_from_vcf_path.sh
+++ generate_commands_automatically_from_vcf_path.sh
@@ -1,168 +1,144 @@
+#!/usr/bin/env bash
 
-#example run: bash generate_commands_from_vcf_path.sh /mnt/stsi/stsi0/raqueld/vcf/SHARE_MESA_c1_flipfix.vcf /mnt/stsi/stsi0/raqueld > MESA_jobs_c2.txt
-#example run: bash generate_commands_from_vcf_path.sh /mnt/stsi/stsi0/raqueld/vcf/SHARE_MESA_c2_flipfix.vcf > MESA_jobs_c1.txt
-#example run: bash generate_commands_from_vcf_path.sh /mnt/stsi/stsi1/raqueld/vcf/ARIC_PLINK_flagged_chromosomal_abnormalities_zeroed_out_flipfix.vcf > ARIC_jobs.txt
+echo
+echo -e " #################################### "
+echo -e " ##                                ## "
+echo -e " ##    Imputation / QC Pipeline    ## "
+echo -e " ##          Torkamani Lab         ## "
+echo -e " ##                                ## "
+echo -e " ##         Author: Raquel Dias    ## "
+echo -e " ##                 Shaun Chen     ## "
+echo -e " ##  Last modified: 12/24/19       ## "
+echo -e " ##                                ## "
+echo -e " #################################### "
+echo 
+echo -e "Usage:    bash script.sh VCF_PATH OUT_ROOT START STOP (CONFIRM) > LOG"
+echo
+echo -e "          script.sh    This script"
+echo -e "          VCF_PATH     Full path of the input vcf file to be QCed/imputed"
+echo -e "          OUT_ROOT     Path of the directory where all the output folders will be created"
+echo -e "          START        First step"
+echo -e "          STOP         Last step"
+echo -e "          CONFIRM      Any string to disable debugging mode"
+echo -e "          LOG          Log report file name"
+echo
+echo -e "Example:  bash generate_commands_from_vcf_path.sh /mnt/stsi/stsi0/raqueld/vcf/SHARE_MESA_c2_flipfix.vcf /mnt/stsi/stsi0/raqueld 0 6 > MESA_jobs_c1.txt"
+echo 
+
+# Default settings
+if [ -z $2 ]; then exit; else myinput=$1; fi
+if [ -z $2 ]; then outroot="/mnt/stsi/stsi0/raqueld"; else outroot=$2; fi
+if [ -z $3 ]; then start_from=999; else start_from=$3; fi
+if [ -z $4 ]; then stop_after=999; else stop_after=$4; fi
+if [ -z $5 ]; then run=0; else run=1; fi
+
+echo "User input: VCF_PATH: $1"
+echo "            OUT_ROOT: $2"
+echo "User option: START=${start_from}, STOP=${stop_after}, RUN=${run}"
+echo 
 
 
-if [ -z $1 ]; then
-    echo -e "Usage:    bash script.sh VCF_PATH OUT_ROOT > LOG"
-    echo
-    echo -e "          script.sh    This script"
-    echo -e "          VCF_PATH     Full path of the input vcf file to be QCed/imputed"
-    echo -e "          OUT_ROOT     Path of the directory where all the output folders will be created"
-    echo -e "          LOG          Log report file name"
-    echo
-    echo -e "Example:  bash generate_commands_from_vcf_path.sh /mnt/stsi/stsi0/raqueld/vcf/SHARE_MESA_c2_flipfix.vcf /mnt/stsi/stsi0/raqueld > MESA_jobs_c1.txt"
-    echo 
-    exit
-fi
-
-if [ -z $2 ]; then
-    outroot="/mnt/stsi/stsi0/raqueld"
-else
-    outroot=$2
-fi
-
-indir=$(dirname $1)
-infile=$(basename $1)
+###################
+## Set Varaibles ##
+###################
+# Parsing system argument
+indir=$(dirname ${myinput})
+infile=$(basename ${myinput})
 prefix=$(echo $infile | sed -e 's/\.vcf$//g')
 
+# Assign submission command line
+job0='qsub 0_check_vcf_build.job -v myinput=${myinput},myoutput=${outroot}/0_check_vcf_build/${prefix}.BuildChecked,copyoutput=no,gz=no -N 0_${prefix}'
+job1='qsub 1_lift_vcfs_to_GRCh37.job -v myinput=${myinput},buildcheck=${outroot}/0_check_vcf_build/${prefix}.BuildChecked,myoutdir=${outroot}/1_lift,copyoutput=no -N 1_${prefix}'
+job2='qsub 2_Genotype_Harmonizer_QC1.job -v myinput=${outroot}/1_lift/${prefix}.lifted_NCBI36_to_GRCh37.bed,myoutdir=${outroot}/2_GH -N 2_${prefix}'
+job3='qsub 3_ancestry_analysis.job -v myinput=${outroot}/2_GH/${prefix}.lifted_NCBI36_to_GRCh37.GH.vcf.gz,myoutdir=${outroot}/3_ancestry -N 3_${prefix}'
+job4='qsub 4_split_QC2.job -v myinput=${outroot}/3_ancestry/${prefix}/${prefix}.lifted_NCBI36_to_GRCh37.GH.ancestry-${anc}.bed,myoutdir=${outroot}/4_split_QC2,geno=0.1,mind=0.05 -N 4_${prefix}'
+job5='qsub 5_phase.job -v myinput=${outroot}/4_split_QC2/${prefix}/${prefix}.lifted_NCBI36_to_GRCh37.GH.ancestry-${anc}.chr${chrom}.bed,myoutdir=${outroot}/5_phase,reftype=HRC -N 5_${prefix}'
+job6='qsub 6_impute.job -v myinput=${outroot}/5_phase/${prefix}/${prefix}.lifted_NCBI36_to_GRCh37.GH.ancestry-${anc}.chr${chrom}.phased.vcf.gz,myoutdir=${outroot}/6_impute,reftype=HRC -N 6_${prefix}'
+
+# empty depend step flag by default
+flag=""
+
+# ancestry array
+ancestry="1 2 3 4 5 mixed"
 
 
-run=0 #whether really submit the jobs or just print out the job commands so you can debug first (recommended to debug first with run=0, then when everything is working, then run with run=1)
-start_from=0 #first step to start running
-stop_after=4 #last step before stop running
+#####################
+## Define Function ##
+#####################
+# declare dictionary for depend flag
+declare -A flag_arr
 
-counter=0
+job() {
+    job="$1" run="$2" step="$3" start_from="$4" stop_after="$5" anc="$6" chrom="$7"
 
-echo "qsub 0_check_vcf_build.job -v myinput=$1,myoutput=$outroot/0_check_vcf_build/$prefix.BuildChecked,copyoutput=no,gz=no -N 0_$prefix"
-if [ $run -eq 1 ] && [ $counter -ge $start_from ]; then
-    job0=$(qsub 0_check_vcf_build.job -v myinput=$1,myoutput=$outroot/0_check_vcf_build/$prefix.BuildChecked,copyoutput=no,gz=no -N 0_$prefix)
-    echo $job0
-    sleep 1
-fi
+    # check if step isin range
+    if [ $step -ge $start_from ] && [ $step -le $stop_after ]; then
+        # get the dependancy job_ID from previous step
+        last_step=$(expr ${step} - 1)
 
-if [ $stop_after -eq $counter ]; then
-    exit
-fi
-
-counter=$((counter+1))
-
-if [ $counter -gt $start_from ]; then
-   flag="-W depend=afterany:$job0"
-else
-   flag=""
-fi
-
-echo "qsub 1_lift_vcfs_to_GRCh37.job -v myinput=$1,buildcheck=$outroot/0_check_vcf_build/$prefix.BuildChecked,myoutdir=$outroot/1_lift,copyoutput=no -N 1_$prefix"
-if [ $run -eq 1 ] && [ $counter -ge $start_from ]; then
-    job1=$(qsub $flag 1_lift_vcfs_to_GRCh37.job -v myinput=$1,buildcheck=$outroot/0_check_vcf_build/$prefix.BuildChecked,myoutdir=$outroot/1_lift,copyoutput=no -N 1_$prefix)
-    echo $job1
-    sleep 1
-fi
-
-if [ $stop_after -eq $counter ]; then
-    exit
-fi
-
-counter=$((counter+1))
-
-if [ $counter -gt $start_from ]; then
-   flag="-W depend=afterany:$job1"
-else
-   flag=""
-fi
-
-echo "qsub 2_Genotype_Harmonizer_QC1.job -v myinput=$outroot/1_lift/$prefix.lifted_NCBI36_to_GRCh37.bed,myoutdir=$outroot/2_GH -N 2_$prefix"
-if [ $run -eq 1 ] && [ $counter -ge $start_from ]; then
-    job2=$(qsub $flag 2_Genotype_Harmonizer_QC1.job -v myinput=$outroot/1_lift/$prefix.lifted_NCBI36_to_GRCh37.bed,myoutdir=$outroot/2_GH -N 2_$prefix)
-    echo $job2
-    sleep 1
-fi
-
-if [ $stop_after -eq $counter ]; then
-    exit
-fi
-
-counter=$((counter+1))
-
-if [ $counter -gt $start_from ]; then
-   flag="-W depend=afterany:$job2"
-else
-   flag=""
-fi
-
-echo "qsub 3_ancestry_analysis.job -v myinput=$outroot/2_GH/$prefix.lifted_NCBI36_to_GRCh37.GH.vcf.gz,myoutdir=$outroot/3_ancestry -N 3_$prefix"
-if [ $run -eq 1 ] && [ $counter -ge $start_from ]; then
-    job3=$(qsub $flag 3_ancestry_analysis.job -v myinput=$outroot/2_GH/$prefix.lifted_NCBI36_to_GRCh37.GH.vcf.gz,myoutdir=$outroot/3_ancestry -N 3_$prefix)
-    echo $job3
-    sleep 1
-fi
-
-if [ $stop_after -eq $counter ]; then
-    exit
-fi
-
-counter=$((counter+1))
-
-if [ $counter -gt $start_from ]; then
-   flag="-W depend=afterany:$job3"
-   flag4="-W depend=afterany:$job3"
-else
-   flag=""
-   flag4=""
-fi
-
-if [ $stop_after -eq 4 ]; then
-
-    for i in $(echo 1 2 3 4 5 mixed); do
-        echo "qsub 4_split_QC2.job -v myinput=$outroot/3_ancestry/$prefix/$prefix.lifted_NCBI36_to_GRCh37.GH.ancestry-$i.bed,myoutdir=$outroot/4_split_QC2,geno=0.1,mind=0.05 -N 4_$prefix"
-
-        if [ $run -eq 1 ] && [ $counter -ge $start_from ]; then
-
-           job4=$(qsub $flag4 4_split_QC2.job -v myinput=$outroot/3_ancestry/$prefix/$prefix.lifted_NCBI36_to_GRCh37.GH.ancestry-$i.bed,myoutdir=$outroot/4_split_QC2,geno=0.1,mind=0.05 -N 4_$prefix)
-           echo $job4
-           sleep 1
+        # if it's step 5, depend flag need ancestry from step 4
+        if [ $step -eq 5 ]; then
+            depend_flag=${flag_arr["${last_step}${anc}"]}
+#             echo depend_flag ${last_step}${anc}
+        # if it's step 6, depend flag need ancestry/chromosome from step 5
+        elif [ $step -eq 6 ]; then
+            depend_flag=${flag_arr["${last_step}${anc}${chrom}"]}
+#             echo depend_flag ${last_step}${anc}${chrom}
+        # else, just get from previous step
+        else
+            depend_flag=${flag_arr["${last_step}"]}
+#             echo depend_flag ${last_step}
         fi
- 
-    done
 
-    exit
-fi
+        # recompose the qsub command line with proper variables
+        job=$(eval echo ${job} ${depend_flag})
+        echo $job
+        
+        # run if debugging mode disabled, recording job_ID
+        if [ $run -eq 1 ]; then
+            job_ID=""
+            
+            # submit the job and save jobID
+            job_ID=$($job)
+            echo ${job0_ID}
+                        
+            sleep 1
+        fi
+        
+        # send jobID to flag array (empty is fine for -W)
+        next_flag="-W depend=afterany:${job_ID}"
+        flag_arr["${step}${anc}${chrom}"]=$next_flag
+#         echo save_step ${step}${anc}${chrom}
 
-for i in $(echo 1 2 3 4 5 mixed); do
-    echo "qsub 4_split_QC2.job -v myinput=$outroot/3_ancestry/$prefix/$prefix.lifted_NCBI36_to_GRCh37.GH.ancestry-$i.bed,myoutdir=$outroot/4_split_QC2,geno=0.1,mind=0.05 -N 4_$prefix"
-
-    if [ $run -eq 1 ] && [ $counter -ge $start_from ]; then
-
-       job4=$(qsub $flag4 4_split_QC2.job -v myinput=$outroot/3_ancestry/$prefix/$prefix.lifted_NCBI36_to_GRCh37.GH.ancestry-$i.bed,myoutdir=$outroot/4_split_QC2,geno=0.1,mind=0.05 -N 4_$prefix)
-       echo $job4
-       sleep 1
-
-       counter=$((counter+1))
-
-       if [ $counter -gt $start_from ]; then
-          flag="-W depend=afterany:$job4"
-       else
-          flag=""
-       fi
-
-       for j in $(seq 1 1 22); do
-
-           echo "qsub 5_phase.job -v myinput=$outroot/4_split_QC2/$prefix/$prefix.lifted_NCBI36_to_GRCh37.GH.ancestry-$i.chr$j.bed,myoutdir=$outroot/5_phase,reftype=HRC -N 5_$prefix"
-           echo "qsub 6_impute.job -v myinput=$outroot/5_phase/$prefix/$prefix.lifted_NCBI36_to_GRCh37.GH.ancestry-$i.chr$j.phased.vcf.gz,myoutdir=$outroot/6_impute,reftype=HRC -N 6_$prefix"
-
-           if [ $run -eq 1 ]; then
-
-               job5=$(qsub $flag 5_phase.job -v myinput=$outroot/4_split_QC2/$prefix/$prefix.lifted_NCBI36_to_GRCh37.GH.ancestry-$i.chr$j.bed,myoutdir=$outroot/5_phase,reftype=HRC -N 5_$prefix)
-               sleep 1
-
-               job6=$(qsub -W depend=afterany:$job5 6_impute.job -v myinput=$outroot/5_phase/$prefix/$prefix.lifted_NCBI36_to_GRCh37.GH.ancestry-$i.chr$j.phased.vcf.gz,myoutdir=$outroot/6_impute/${prefix}/ancestry_${i},reftype=HRC -N 6_$prefix)
-               sleep 1
-
-           fi
-
-       done
+    else
+        if [ -z $anc ]; then anc="ALL"; fi
+        if [ -z $chrom ]; then chrom="ALL"; fi
+        echo "# Skip step-${step}_ancestry-${anc}_chr${chrom}"
     fi
+}
+
+
+#################
+## Main Script ##
+#################
+job "$job0" "$run" 0 "$start_from" "$stop_after"
+job "$job1" "$run" 1 "$start_from" "$stop_after"
+job "$job2" "$run" 2 "$start_from" "$stop_after"
+job "$job3" "$run" 3 "$start_from" "$stop_after"
+
+for anc in $ancestry; do
+    job "$job4" "$run" 4 "$start_from" "$stop_after" "$anc"
+done
+
+for anc in $ancestry; do
+    for chrom in {1..22}; do
+        job "$job5" "$run" 5 "$start_from" "$stop_after" "$anc" "$chrom"
+    done
+done
+
+for anc in $ancestry; do
+    for chrom in {1..22}; do
+        job "$job6" "$run" 6 "$start_from" "$stop_after" "$anc" "$chrom"
+    done
 done
 
